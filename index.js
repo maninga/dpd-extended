13f86e1953f391ac29f12486c58c391c79aa58cc
David KEITA <maninga@gmail.com>
2015-09-19 13:04:54 +0200

    version maj; rename module arg to resource.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 	var _run = Script.prototype.run;
 	Script.prototype.run = function(ctx, domain, fn) {
 		if (typeof domain === 'object') {
-			domain.require = function(module) {
-				return require(module);
+			domain.require = function(resource) {
+				return require(resource);
 			};
 			domain.context = function() { // access Context via context()
 				return ctx;
 			};
 		}
 		_run.call(this, ctx, domain, fn);
-	}
+	};
 })(require('deployd/lib/script'));
